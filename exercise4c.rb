9c85bafd44daf459e8dc25bc2997a919504a9488
KYLE LIPTON <lipton.kyle@gmail.com>
2017-10-31 02:48:46 -0400

    Add Assignment 4, question 3

---
+++ exercise4c.rb
@@ -0,0 +1,15 @@
+# Question 3 of exercise 4
+puts "Hello! What's your your name"
+
+your_name = gets.chomp
+
+my_name = "Kyle"
+
+if your_name == my_name
+	puts "Nice! We have the same name!"
+
+else
+
+	puts "Ah shucks! We don't have the same name."
+
+end
