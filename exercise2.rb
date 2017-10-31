1497977f7e8afffc4dcebd0f4d106dfe83d07642
KYLE LIPTON <lipton.kyle@gmail.com>
2017-10-30 22:31:04 -0400

    Exercise 3 ruby greeter

--- exercise2.rb
+++ exercise2.rb
@@ -12,3 +12,9 @@ puts "#{45628 * 7839}"
 
 #What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
 puts (10 < 20 && 30 < 20) || !(10 == 11)
+
+name = "Sandra"
+greeting = "Hello #{name}! It's good to see you again."
+mission = "Your mission, should you choose to accept it..."
+
+puts "#{greeting} #{mission}"
---
+++ exercise3.rb
@@ -0,0 +1,4 @@
+#Exercise 3
+puts "What is your name?"
+user_name = gets
+puts "Hello, Doctor #{user_name}"
